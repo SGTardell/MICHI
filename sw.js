36d526c221db2511316195406968a5fefb627ad3
SGTardell <sgt@tardell.com>
2026-09-01 15:55:13 -0700

    Re-enable Service Worker in sw.js for PWA Share Target and enable Desktop Bookmarklet dragging

--- sw.js
+++ sw.js
@@ -1,10 +1,16 @@
+const CACHE_NAME = 'michi-pwa-v2';
+
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
-      .then(() => self.registration.unregister())
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  // Network-first fetch handler so updates are instant while keeping PWA Share Target registered
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
